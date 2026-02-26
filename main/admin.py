0b6f97dca27b972f2108f2b28908a3b5b37c5191
Lavandazz <167910387+Lavandazz@users.noreply.github.com>
2026-02-26 23:28:07 +0300

    Исправлено отображение фронтенда. Настроен слайдер фото. Адаптировано отображение на телефоне. Настроена админка

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from adminsortable2.admin import SortableAdminMixin
+from django.utils.html import format_html
 
 from main.models import SpaceImage
 
@@ -9,12 +10,13 @@ from main.models import SpaceImage
 
 @admin.register(SpaceImage)
 class SpaceImageAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ('title', 'image')
+    list_display = ('title', 'image', 'image_preview')
     search_fields = ('title',)
-
 
     def image_preview(self, obj):
         if obj.image:
-            return f'<img src="{obj.image.url}" style="max-height: 50px; max-width: 50px;" />'
+            return format_html (f'<img src="{obj.image.url}" style="max-height: 100px; max-width: 100px;" />')
         return "Нет изображения"
+
+    image_preview.short_description = 'Превью изображения'
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from main.models import SpaceImage
+
+
 # Create your views here.
 
 
@@ -7,8 +10,10 @@ def index(request):
     title = 'Космическое агентство'
     description = ('Национальное управление по аэронавтике и исследованию космического пространства — ведомство, '
                    'относящееся к федеральному правительству США и подчиняющееся непосредственно президенту США.')
+    slides = SpaceImage.objects.all()
     context = {
         'title': title,
         'description': description,
+        'slides': slides
     }
     return render(request=request, context=context, template_name="index.html")
